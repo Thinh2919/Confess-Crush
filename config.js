4d92560d3ea370336ffae99421a74c8af853555f
Thinh2919 <106761138+Thinh2919@users.noreply.github.com>
2022-06-02 22:31:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "gửi choa cục cưng cụa anh",
+    introTitle: 'Thẻo Ngu cụa anh',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
